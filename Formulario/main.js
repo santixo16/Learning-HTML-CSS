2a7cc15580a3d47aec1a7d7f2b90e62e25b1a903
santixo16 <santixo2022@gmail.com>
2022-06-23 18:41:47 -0500

    first commit

---
+++ Formulario/main.js
@@ -0,0 +1,14 @@
+function suma(num1, num2){
+    result=num1+num2;
+    console.log(result);
+}
+
+suma(5, 8);
+
+function saludo(){
+    alert("Bienvenido a este formulario almacenado en un servidor local");
+    nombre=prompt("Escriba su nombre para continuar");
+    console.log("Bienvenid@ "+nombre);
+}
+
+saludo();
