c1e3656557e6b880bf704bd0fd04f5f3db7c38ab
santixo16 <santixo2022@gmail.com>
2022-06-26 15:37:36 -0500

    add operators

---
+++ JS/Operators/index.js
@@ -0,0 +1,69 @@
+//Mathematical operators
+//adition
+const suma = 1 + 2
+
+//substraction
+const resta = 1 - 2
+
+//multiplication
+const mult = 2 * 3
+
+//division
+const div = 9 / 3
+
+console.log(suma, resta, mult, div);
+
+//module
+const modulo = 10 % 3
+console.log(modulo);
+
+let num = 5
+num-- //We substract 1 to the value of num
+num++ //We add 1 to the value of num
+num+=5 //We add 5 to the value of num
+num-=5 //We substract 5 to the value of num
+console.log(num);
+
+//Comparation Operators
+
+const result1 = 5 === 6
+console.log(result1);
+
+const result2 = 5 == 6
+console.log(result2);
+
+const result3 = 5 == 5
+console.log(result3);
+
+const result4 = 5 == '5'
+console.log(result4);
+
+const result5 = 5 ==='5'
+console.log(result5);
+
+const result6 = 5 < 6
+console.log(result6);
+
+const result7 = 5 > 4
+console.log(result7);
+
+const result8 = 5 !== '5'
+console.log(result8);
+
+const result9 = 5 != '5'
+console.log(result9);
+
+//Logical operators
+//or (||), and (&&), not (!)
+
+//or
+const resultOr = false || true
+console.log(resultOr);
+
+//and
+const resultAnd = true && true && false
+console.log(resultAnd);
+
+//not
+const resultNot = !true
+console.log(resultNot);
