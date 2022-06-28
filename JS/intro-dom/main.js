8cc053838d41a3526c8bdadd800ccc99b2c978cc
santixo16 <santixo2022@gmail.com>
2022-06-27 23:07:53 -0500

    add dom

---
+++ JS/intro-dom/main.js
@@ -0,0 +1,4 @@
+window.onload = () => { //This is to wait until the HTML charge fully so we can use it's elements
+    const parrafo = document.getElementById("text")
+    parrafo.innerHTML = '<li>Element 1</li><li>Element 2</li>'
+}
